205a1874eb95c89a933699900b4c351942d3fe0b
Angela Yen <ayen10@gmail.com>
2014-05-26 16:48:20 -0700

    add index, new, and create methods to expenses controller and flesh out expenses index page with table for all expenses (needs to be updated to all expenses within the current month)

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,3 +1,27 @@
 class ExpensesController < ApplicationController
 
+
+  def new
+    @expense = Expense.new
+    @user = User.find(params[:user_id])
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @expense = @user.expenses.new(expense_params)
+    if @expense.save
+      respond_to do |format|
+        format.html { redirect_to user_expenses_path(@user) }
+        format.js
+      end
+    else
+      render 'new_user_expense_path(@user)'
+    end
+  end
+
+
+private
+  def expense_params
+    params.require(:expense).permit(:amount, :note, :user_id)
+  end
 end
